f2c7f76d653cae0af5e29ee50ceebb4db55eb807
ZanyXDev <zanyxdev@gmail.com>
2018-02-26 13:45:18 +0300

    small fix for build

--- app/src/main/java/io/github/zanyxdev/smsredirect/MainActivity.java
+++ app/src/main/java/io/github/zanyxdev/smsredirect/MainActivity.java
@@ -1,11 +1,10 @@
 package io.github.zanyxdev.smsredirect;
 
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements {
+
+public class MainActivity extends AppCompatActivity  {
 
     private static final String DEBUG_TAG = "SMSREDIRECT";
 
